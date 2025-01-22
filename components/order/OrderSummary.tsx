faaab18ea8345d1ff59314fca476fc2633f3d724
Thomas_465 <160943727+Thomas465xd@users.noreply.github.com>
2025-01-22 16:40:55 -0300

    *Basic NextJS 14 structure for French Coffe a Food Ordering App -Cleaned up the structure -Added new pages for products and orders -Added new components for order sidebar and order summary

---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,7 @@
+export default function OrderSummary() {
+    return (
+        <aside className="lg:h-screen lg:overflow-scroll md:w-72 lg:w-96 p-5 bg-white">
+            <h1 className="text-4xl text-center font-black">Order Summary</h1>
+        </aside>
+    )
+}
